ebe6f2d8842f7dcc8df11f939df9c49d9fffbbda
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-08-17 09:39:02 +0200

    Add project files.

---
+++ Northwind_WebAPI.Entities/Invoice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Northwind_WebAPI.Entities
+{
+    public class Invoice
+    {
+        int id;
+        string shipName;
+        string shipAddress;
+        string shipCity;
+        string shipRegion;
+        string shipPostalCode;
+        string shipCountry;
+        string customerId;
+        string customerName;
+        string address;
+        string city;
+        string region;
+        string postalCode;
+        string country;
+        string salesPerson;
+        int orderId;
+        DateTime orderDate;
+        DateTime requiredDate;
+        DateTime shippedDate;
+        string shipperName;
+        int productId;
+        string productName;
+        decimal unitPrice;
+        int quantity;
+        decimal discount;
+        decimal extendedPrice;
+        decimal freight;
+    }
+}
